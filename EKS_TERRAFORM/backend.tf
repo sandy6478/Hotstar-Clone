395beef64856aefeae54bf72defc3c0c16b4cc0c
sandy6478 <101122409+sandy6478@users.noreply.github.com>
2024-01-12 13:12:31 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "practicegitactionbucket" # Replace with your actual S3 bucket name
+    bucket = "hotstarsandy-clone-terraform" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
